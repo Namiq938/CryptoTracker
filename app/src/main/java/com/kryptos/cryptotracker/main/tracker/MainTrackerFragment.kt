5fead1dc062b4609bddec7fb65709b77c73a86cf
Namig Gadirov <namig.gadirov@guavapay.com>
2021-12-13 11:19:46 +0400

    main screen ui elements added

--- app/src/main/java/com/kryptos/cryptotracker/main/tracker/MainTrackerFragment.kt
+++ app/src/main/java/com/kryptos/cryptotracker/main/tracker/MainTrackerFragment.kt
@@ -1,14 +1,9 @@
 package com.kryptos.cryptotracker.main.tracker
 
-import android.net.Uri
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.result.contract.ActivityResultContracts
 import com.kryptos.core.BaseFragment
-import com.kryptos.cryptotracker.R
 import com.kryptos.cryptotracker.databinding.FragmentMainTrackerBinding
 
 class MainTrackerFragment : BaseFragment<MainTrackerViewModel, FragmentMainTrackerBinding, Unit, MainTrackerEvent>(), View.OnClickListener {
